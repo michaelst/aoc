82f9b4094dbf423b7043aa92f9a5feccb7351990
Michael St Clair <michaelst57@gmail.com>
2023-12-03 12:35:38 -0700

    use tuple instead

--- lib/advent_of_code/day_03a.ex
+++ lib/advent_of_code/day_03a.ex
@@ -4,17 +4,25 @@ defmodule AdventOfCode.Day03A do
 
   def part2(args) do
     lines = String.split(args, "\n", trim: true)
+    total_rows = length(lines)
 
-    character_list = Enum.map(lines, &String.split(&1, "", trim: true))
-    character_map = list_to_map(character_list)
+    [first_row | _rows] = character_list = Enum.map(lines, &String.split(&1, "", trim: true))
+    total_cols = length(first_row)
 
-    Enum.reduce(character_map, 0, fn {row, row_map}, total ->
-      Enum.reduce(row_map, total, fn
-        {col, symbol}, row_total when symbol in @symbols ->
-          find_gears(row_map, character_map, row, col) + row_total
+    character_tuple = list_to_tuple(character_list)
 
-        _item, row_total ->
-          row_total
+    Enum.reduce(0..(total_rows - 1), 0, fn row, total ->
+      row_tuple = elem(character_tuple, row)
+
+      Enum.reduce(0..(total_cols - 1), total, fn
+        col, row_total ->
+          case elem(row_tuple, col) do
+            symbol when symbol in @symbols ->
+              find_gears(row_tuple, character_tuple, row, col) + row_total
+
+            _other ->
+              row_total
+          end
       end)
     end)
   end
@@ -34,19 +42,31 @@ defmodule AdventOfCode.Day03A do
     end)
   end
 
-  defp find_gears(row_map, character_map, row, col) do
-    previous_line_map = Map.get(character_map, row - 1) || Map.new()
-    next_line_map = Map.get(character_map, row + 1) || Map.new()
+  def list_to_tuple(list) do
+    list
+    |> Enum.map(&List.to_tuple/1)
+    |> List.to_tuple()
+  end
+
+  def get_coordinate(tuple, index) do
+    elem(tuple, index)
+  rescue
+    _error -> nil
+  end
+
+  defp find_gears(row_tuple, character_tuple, row, col) do
+    previous_line_tuple = get_coordinate(character_tuple, row - 1) || {}
+    next_line_tuple = get_coordinate(character_tuple, row + 1) || {}
 
     [
       # before symbol
-      parse_number_before_col(row_map, col),
+      parse_number_before_col(row_tuple, col),
       # after symbol
-      parse_number_after_col(row_map, col),
+      parse_number_after_col(row_tuple, col),
       # row before
-      number_from_adjacent_line(previous_line_map, col),
+      number_from_adjacent_line(previous_line_tuple, col),
       # row after
-      number_from_adjacent_line(next_line_map, col)
+      number_from_adjacent_line(next_line_tuple, col)
     ]
     |> List.flatten()
     |> Enum.reject(&(&1 == ""))
@@ -59,72 +79,67 @@ defmodule AdventOfCode.Day03A do
 
   defp add_gears(_gears), do: 0
 
-  defp parse_number_before_col(row_map, col, acc \\ [])
+  defp parse_number_before_col(row_tuple, col, acc \\ [])
 
-  defp parse_number_before_col(_row_map, _col, [first, second, third]) do
+  defp parse_number_before_col(_row_tuple, _col, [first, second, third]) do
     first <> second <> third
   end
 
-  defp parse_number_before_col(row_map, col, acc) do
-    case Map.get(row_map, col - 1) do
+  defp parse_number_before_col(row_tuple, col, acc) do
+    case get_coordinate(row_tuple, col - 1) do
       value when value in @numbers ->
-        parse_number_before_col(row_map, col - 1, [value | acc])
+        parse_number_before_col(row_tuple, col - 1, [value | acc])
 
       _value ->
         Enum.join(acc)
     end
   end
 
-  defp parse_number_after_col(row_map, col, acc \\ [])
+  defp parse_number_after_col(row_tuple, col, acc \\ [])
 
-  defp parse_number_after_col(_row_map, _col, [first, second, third]) do
+  defp parse_number_after_col(_row_tuple, _col, [first, second, third]) do
     first <> second <> third
   end
 
-  defp parse_number_after_col(row_map, col, acc) do
-    case Map.get(row_map, col + 1) do
+  defp parse_number_after_col(row_tuple, col, acc) do
+    case get_coordinate(row_tuple, col + 1) do
       value when value in @numbers ->
-        parse_number_after_col(row_map, col + 1, acc ++ [value])
+        parse_number_after_col(row_tuple, col + 1, acc ++ [value])
 
       _value ->
         Enum.join(acc)
     end
   end
 
-  defp number_from_adjacent_line(row_map, col) do
-    col_minus_one = col - 1
-    col_plus_one = col + 1
-
-    %{
-      ^col_minus_one => first,
-      ^col => second,
-      ^col_plus_one => third
-    } = row_map
+  defp number_from_adjacent_line(row_tuple, col) do
+    first = get_coordinate(row_tuple, col - 1)
+    second = get_coordinate(row_tuple, col)
+    third = get_coordinate(row_tuple, col + 1)
 
     case {first in @numbers, second in @numbers, third in @numbers} do
       {true, true, true} ->
         first <> second <> third
 
       {true, true, false} ->
-        parse_number_before_col(row_map, col + 1)
+        parse_number_before_col(row_tuple, col + 1)
 
       {false, true, true} ->
-        parse_number_after_col(row_map, col - 1)
+        parse_number_after_col(row_tuple, col - 1)
 
       {false, true, false} ->
         second
 
       {true, false, true} ->
-        before_number = parse_number_before_col(row_map, col)
-        after_number = parse_number_after_col(row_map, col)
+        before_number = parse_number_before_col(row_tuple, col)
+        after_number = parse_number_after_col(row_tuple, col)
 
         [before_number, after_number]
 
       {true, false, false} ->
-        parse_number_before_col(row_map, col)
+        parse_number_before_col(row_tuple, col)
 
       {false, false, true} ->
-        parse_number_after_col(row_map, col)
+        parse_number_after_col(row_tuple, col)
 
       {false, false, false} ->
         []

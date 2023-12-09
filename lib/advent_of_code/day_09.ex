99b52deb5aadde824ad8c98a41a85b6cdaf6c71f
Michael St Clair <michaelst57@gmail.com>
2023-12-08 22:44:20 -0700

    day 9

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,57 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args) do
+    String.split(args, "\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reverse()
+    end)
+    |> Enum.map(&process_sequence/1)
+    |> Enum.map(&hd/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    String.split(args, "\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reverse()
+    end)
+    |> Enum.map(&process_sequence/1)
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.map(&process_sequence/1)
+    |> Enum.map(&hd/1)
+    |> Enum.sum()
+  end
+
+  defp process_sequence(sequence, acc \\ []) do
+    next_sequence =
+      differences(sequence)
+      |> Enum.reverse()
+
+    if Enum.all?(next_sequence, &(&1 == 0)) do
+      predict([next_sequence, sequence | acc])
+    else
+      process_sequence(next_sequence, [sequence | acc])
+    end
+  end
+
+  defp predict([final]), do: final
+
+  defp predict([first, second | tail]) do
+    next = [hd(first) + hd(second) | second]
+
+    predict([next | tail])
+  end
+
+  defp differences(sequence, acc \\ [])
+
+  defp differences([_], acc), do: acc
+
+  defp differences([first | [second | _] = tail], acc) do
+    differences(tail, [first - second | acc])
   end
 end
